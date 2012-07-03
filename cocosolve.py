08b2ed1b483a2a6b98c31724da67c7eb5a4671ae
Iñaki Silanes <isilanes@gmail.com>
2012-07-03 09:48:54 +0200

    * use argparse for options * allow for multiple input files one after the other * break on solution/error, not sys.exit()

--- cocosolve.py
+++ cocosolve.py
@@ -1,53 +1,67 @@
 #!/usr/bin/python
 
 import sys
-import modules.core as C
+import argparse
+
+from modules import core
 
 #------------------------------------------------------------------------------#
 
-verbose = True
+parser = argparse.ArgumentParser()
 
-# Initialize cube:
-cube = C.Cube()
+parser.add_argument('infiles',
+        metavar="infile",
+        nargs="+",
+        help="one or more input file(s)")
 
-# Read pieces from file:
-if len(sys.argv) > 1:
-    cube.read(sys.argv[1])
-else:
-    sys.exit()
+parser.add_argument('-v',
+        dest='verbose',
+        action="store_true",
+        default=False,
+        help="be verbose")
 
-if verbose:
-    cube.show()
-# Total number of iterations:
-niter = 0
+args = parser.parse_args()
 
-remaining = True
-while remaining:
-    if verbose:
-        cube.show_status()
+#------------------------------------------------------------------------------#
 
-    niter += 1
-    if niter > 50000:
-        print("Max reached\n")
-        cube.show()
-        sys.exit()
-
-    # It fits?:
-    if cube.fits():
-        if cube.ipos > 4:
-            # Then it's solved:
-            print("Solution:\n")
-            cube.show()
-            cube.showdata(niter)
-            sys.exit()
-        else:
-            # Move on to next position:
-            cube.forward()
-    else:
-        # Try next combination:
-        remaining = cube.next()
+for infile in args.infiles:
+    # Initialize cube:
+    cube = core.Cube()
+    cube.read(infile)
     
-    # If we exhausted all combinations thus far, we need to backtrack:
-    if not remaining:
-        cube.backtrack()
-        remaining = True
+    if args.verbose:
+        cube.show()
+        
+    # Total number of iterations:
+    niter = 0
+    
+    remaining = True
+    while remaining:
+        if args.verbose:
+            cube.show_status()
+
+        niter += 1
+        if niter > 50000:
+            print("Max reached\n")
+            cube.show()
+            sys.exit()
+    
+        # It fits?:
+        if cube.fits():
+            if cube.ipos > 4:
+                # Then it's solved:
+                print("Solution:\n")
+                cube.show()
+                cube.showdata(niter)
+                break
+            else:
+                # Move on to next position:
+                cube.forward()
+        else:
+            # Try next combination:
+            remaining = cube.next()
+        
+        # If we exhausted all combinations thus far, we need to backtrack:
+        if not remaining:
+            cube.backtrack()
+            remaining = True
